338437fc1be7e2e2069c0b8b4d49849c43639fdd
Goitseone-Themba <goitseonerozthemba@gmail.com>
2024-11-09 21:39:02 +0200

    update main

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+mod models;
+mod routes;
+
+use routes::student_routes::*;
+use routes::room_routes::*;
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![
+            register_student,
+            get_students,
+            get_rooms,
+            allocate_rooms,
+            deallocate_rooms
+        ])
 }
